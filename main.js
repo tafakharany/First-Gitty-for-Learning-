8da1c58922b469edd85ec51d14f45c764224e3dd
tafakharany <tafakharany@outlook.com>
2017-08-30 11:49:00 +0200

    5th commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function scarm() {
-    var x = document.getElementById("dv");
-    var y = 5,
+    var x = document.getElementById("xx"),
+        y = 5,
         z = 20,
         sum = y + z;
 
     console.log(sum);
-
+    x.innerHTML = sum;
 }
